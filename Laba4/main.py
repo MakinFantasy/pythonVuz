b7147add9537b0f82beff2d5ceea58eaf2366f0f
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-12-02 00:16:42 +0300

    laba4 task1 finished

---
+++ Laba4/main.py
@@ -0,0 +1,41 @@
+from random import uniform
+from pprint import pprint
+
+
+def init_array(length):
+    array = []
+    try:
+        for i in range(length):
+            array.append(uniform(-10, 10))
+    except Exception as e:
+        print(e)
+        print(e.__doc__)
+        exit(1)
+    return array
+
+
+def negative_numbers_multiplication (array):
+    negative_numbers = [i for i in array if i < 0]
+    negative_result = 1
+    for num in negative_numbers:
+        negative_result *= num
+    return negative_result
+
+
+def before_max_summary (array):
+    max_el = array.index(max(array))
+    before_max_el = []
+    for i in range(0, max_el):
+        if (array[i] >= 0):
+            before_max_el.append(array[i])
+    return sum(before_max_el)
+
+
+
+if __name__ == '__main__':
+    LENGTH = int(input('Input array lenght: '))
+    userNums = init_array(LENGTH)
+    pprint(userNums)
+    print("Произведение отрицательных элементов: " + str(negative_numbers_multiplication(userNums)))
+    print("Сумма положительных эл-ов, расположенных до максимального: " + str(before_max_summary(userNums)))
+    
---
+++ Laba4/test.py
@@ -0,0 +1,7 @@
+lst = [i for i in range(11)]
+print(lst)
+
+print(lst.index(5))
+
+for j in range(lst.index(5)):
+    print(lst[j])
