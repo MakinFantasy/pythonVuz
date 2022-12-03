2cdb7ff594016559f3091831801b2d529e2ab838
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-12-03 18:43:33 +0300

    finished laba4, need docs

--- Laba4/main.py
+++ Laba4/main.py
@@ -15,27 +15,42 @@ def init_array(length):
 
 
 def negative_numbers_multiplication(array):
-    negative_numbers = [i for i in array if i < 0]
-    negative_result = 1
-    for num in negative_numbers:
-        negative_result *= num
-    return negative_result
+    try:
+        negative_numbers = [i for i in array if i < 0]
+        negative_result = 1
+        for num in negative_numbers:
+            negative_result *= num
+        return negative_result
+    except Exception as e:
+        print(e)
+        print(e.__doc__)
+        exit(1)
 
 
 def before_max_summary(array):
-    max_el = array.index(max(array))
-    before_max_el = []
-    for i in range(0, max_el):
-        if array[i] >= 0:
-            before_max_el.append(array[i])
-    return sum(before_max_el)
+    try:
+        max_el = array.index(max(array))
+        before_max_el = []
+        for i in range(0, max_el):
+            if array[i] >= 0:
+                before_max_el.append(array[i])
+        return sum(before_max_el)
+    except Exception as e:
+        print(e)
+        print(e.__doc__)
+        exit(1)
 
 
 if __name__ == '__main__':
-    LENGTH = int(input('Input array lenght: '))
-    userNums = init_array(LENGTH)
-    pprint(userNums)
-    print("Произведение отрицательных элементов: " + str(negative_numbers_multiplication(userNums)))
-    print("Сумма положительных эл-ов, расположенных до максимального: " + str(before_max_summary(userNums)))
-    userNums.reverse()
-    print(userNums)
+    try:
+        LENGTH = int(input('Input array lenght: '))
+        userNums = init_array(LENGTH)
+        pprint(userNums)
+        print("Произведение отрицательных элементов: " + str(negative_numbers_multiplication(userNums)))
+        print("Сумма положительных эл-ов, расположенных до максимального: " + str(before_max_summary(userNums)))
+        userNums.reverse()
+        print(userNums)
+    except Exception as e:
+        print(e)
+        print(e.__doc__)
+        exit(1)
