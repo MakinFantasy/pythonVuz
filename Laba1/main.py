0020649b72a221246d906bd254d1eb709da6a3e8
MakinFantasy <beingp0z1t1v3@gmail.com>
2022-12-08 00:39:59 +0300

    docs for laba5

--- Laba1/main.py
+++ Laba1/main.py
@@ -4,28 +4,41 @@ from math import sin, cos, pi, sqrt
 
 
 def z1():
-    print('First exp. test started')
-    counter = 0
-    while counter < 5:
-        a = float(input())
-        z = cos(a) + sin(a) + cos(3 * a) + sin(3 * a)
-        print(z)
-        counter += 1
-    print('First exp. test finished')
+    try:
+        print('First exp. test started')
+        counter = 0
+        while counter < 5:
+            a = float(input())
+            if (1 + a + a**2 == 0) or (2*a + a**2 == 0) or (1-a+a**2 == 0) or (2*a-a**2 == 0):
+                print('ОДЗ!!!')
+                counter += 1
+            else:
+                z = ( ( (1 + a + a**2) / (2*a + a**2))  + 2 - ( (1 - a + a**2) / (2*a - a**2) ) ) * (5 - 2*a**2)
+                print("z(a)" + str(z))
+            counter += 1
+        print('First exp. test finished')
+    except Exception as e:
+        print(e)
+        print(e.__doc__)
+        exit(1)
 
 
 def z2():
-    print('Second exp. test started')
-    counter = 0
-    while counter < 5:
-        a = float(input())
-        z = 2 * sqrt(2) * cos(a) * sin((pi/4) + 2 * a)
-        print(z)
-        counter += 1
-    print('Second exp. test finished')
+    try:
+        print('Second exp. test started')
+        counter = 0
+        while counter < 5:
+            a = float(input())
+            z = (4 - a**2) / 2
+            print(z)
+            counter += 1
+        print('Second exp. test finished')
+    except Exception as e:
+        print(e)
+        print(e.__doc__)
+        exit(1)
 
 
 if __name__ == '__main__':
     z1()
     z2()
-
